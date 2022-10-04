40a7c96de18a623336a8b4dea425e41c33638384
Ramzi Al-Dorikhim <ramzik149@gmail.com>
2022-10-04 22:02:59 +0300

    s

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,7 @@ import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(toCamelCase("the-stealth-warrior"));
-    }
 
-    static String toCamelCase(String str){
-        String[] words = str.split("[-_]");
-        return Arrays.stream(words, 1, words.length)
-                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
-                .reduce(words[0], String::concat);
-    }
+
+
 }
